8b1e4561ab91adbd08eaaf974c7b928ff577c25c
philippesikora <p.sikora@hotmail.fr>
2020-01-31 15:39:34 +0100

    Update ESP-01_OLED_SERVER_V31.ino

--- ESP-01_OLED_SERVER_V31.ino
+++ ESP-01_OLED_SERVER_V31.ino
@@ -1,13 +1,13 @@
 // Device: ESP01 or WEMOS D1 
 // Output L1, L2 et L3 active powers from Triphase Router 
 // Solar Rx / Tx in JSON format to OLED 128*64 and server web
-// Install SSD1306Ascii, Arduinojson 5.13.5 
+// Install SSD1306Ascii, Arduinojson 6.14.1 
 
 #include <Wire.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h> 
-#include <ArduinoJson.h>             // Arduinojson 5.13.5
+#include <ArduinoJson.h>             // Arduinojson 6.14.1
 #include <ESP8266HTTPClient.h>
 
 #include <WiFiClient.h>
@@ -296,10 +296,9 @@ void oled_display () {
  
 
 void parsing_json () {
-  StaticJsonBuffer<200> jsonBuffer;
+  StaticJsonDocument<200> jsonBuffer;
   //char json[] = "{\"L1\":30,\"L2\":40,\"L3\":100,\"LOAD_0\":0,\"LOAD_1\":0,\"LOAD_2\":1}";  //pour les tests
-  phases_info=String(json);
-  JsonObject& object = jsonBuffer.parseObject(phases_info);
+  DeserializationError error = deserializeJson(jsonBuffer, json);
   
   if (!object.success()) {
     L1=999;L2=999;L3=999;ACTIVE_POWER=999; routing="ERROR JSON"; // if parsing failed L1=999;L2=999;L3=999;
@@ -313,15 +312,15 @@ void parsing_json () {
     delay(3000);   
    }
 
-  else {                                       // if JSON parsing OK then get L1, L2 and L3 values
-  L1= object["L1"];                            // L1 active power
-  L2= object["L2"];                            // L2 active power
-  L3= object["L3"];                            // L3 active power
-  ACTIVE_POWER= L1 + L2 + L3;                  // Total active power
-  LOAD_0= object["LOAD_0"];                    // LOAD_0 information
-  LOAD_1= object["LOAD_1"];                    // LOAD_1 information
-  LOAD_2= object["LOAD_2"];                    // LOAD_2 information
-  OFF_PEAK_TARIFF= object["OFF_PEAK_TARIFF"];  // OFF_PEAK_TARIFF
+  else {                                           // if JSON parsing OK then get L1, L2 and L3 values
+  L1= jsonBuffer["L1"];                            // L1 active power
+  L2= jsonBuffer["L2"];                            // L2 active power
+  L3= jsonBuffer["L3"];                            // L3 active power
+  ACTIVE_POWER= L1 + L2 + L3;                      // Total active power
+  LOAD_0= jsonBuffer["LOAD_0"];                    // LOAD_0 information
+  LOAD_1= jsonBuffer["LOAD_1"];                    // LOAD_1 information
+  LOAD_2= jsonBuffer["LOAD_2"];                    // LOAD_2 information
+  OFF_PEAK_TARIFF= jsonBuffer["OFF_PEAK_TARIFF"];  // OFF_PEAK_TARIFF
 
   //myBroker.publish("broker/ACTIVE_POWER", (String)ACTIVE_POWER);   // need to convert to String to publish on MQTT 
   
@@ -426,7 +425,7 @@ void setup()
   oled.println ("***********");
   delay (3000); 
 
-  ESP.wdtEnable(3000);                                     // Watchdog enable   
+  ESP.wdtEnable(5000);                                     // Watchdog enable   
 
   startTime_OLED = millis();                               // startTime_OLED
   startTime_p_routed = millis();                           // starttime_p_routed
